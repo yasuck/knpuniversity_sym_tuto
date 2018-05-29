d1686a2812188f663829861d9ec4e3835b13d839
Yasu <yassine_vh@hotmail.com>
2018-05-30 00:28:59 +0200

    AJAX

--- public/js/article_show.js
+++ public/js/article_show.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
     $('.js-like-article').on('click', function(e) {
         e.preventDefault();
-
         var $link = $(e.currentTarget);
         $link.toggleClass('fa-heart-o').toggleClass('fa-heart');
-
         $.ajax({
             method: 'POST',
             url: $link.attr('href')
-        }).done(function(data) {
+        }).done(function (data){
             $('.js-like-article-count').html(data.hearts);
-        })
+        });
     });
 });
+
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,20 +12,21 @@ namespace App\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response("First page");
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug){
         /*dump($slug, $this);*/
@@ -36,7 +37,17 @@ class ArticleController extends AbstractController
         ];
         return $this->render("article/show.html.twig", [
             'title' => ucwords(str_replace('-',' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug){
+        //TODO DB
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
